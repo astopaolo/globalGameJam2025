cbb51d74af60445cf09a90de5ce091b33af51e79
Paolo Astorino <astopaolo@gmail.com>
2025-01-26 12:08:32 +0100

    Update ScorePanel.java

--- globalGameJam/src/main/java/it/gamejam/truncate/bubblenap/ui/ScorePanel.java
+++ globalGameJam/src/main/java/it/gamejam/truncate/bubblenap/ui/ScorePanel.java
@@ -3,15 +3,23 @@ package it.gamejam.truncate.bubblenap.ui;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.JPanel;
 
+import it.gamejam.truncate.bubblenap.ui.audio.SimpleAudioPlayer;
+import it.gamejam.truncate.bubblenap.ui.audio.SoundProvider;
 import it.gamejam.truncate.bubblenap.ui.img.ImageLoader;
 
 public class ScorePanel extends JPanel {
 	private static final long serialVersionUID = -2020644767984510521L;
 
 	private static Image scoreBackground = ImageLoader.getImageScoreBackground();
+	private static Image back = ImageLoader.getImageBack();
+	private static final int BACK_X = (1280 / 2) - 150;
+	private static final int BACK_Y = 643;
 	MainFrame frame;
 
 	public ScorePanel(final MainFrame frame) {
@@ -22,12 +30,46 @@ public class ScorePanel extends JPanel {
 
 		requestFocus();
 
+		addMouseMotionListener(new MouseMotionAdapter() {
+
+			@Override
+			public void mouseMoved(final MouseEvent e) {
+				// Back Start
+				if ((e.getX() >= BACK_X) && (e.getX() <= (BACK_X + ImageLoader.getImageBack().getWidth(null)))
+						&& (e.getY() >= BACK_Y)
+						&& (e.getY() <= (BACK_Y + ImageLoader.getImageBack().getHeight(null)))) {
+					back = ImageLoader.getImageBackPressed();
+				} else {
+					back = ImageLoader.getImageBack();
+					// Back End
+				}
+
+				repaint();
+			}
+		});
+
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(final MouseEvent e) {
+				// pulsante back
+				if ((e.getX() >= BACK_X) && (e.getX() <= (BACK_X + ImageLoader.getImageBack().getWidth(null)))
+						&& (e.getY() >= BACK_Y)
+						&& (e.getY() <= (BACK_Y + ImageLoader.getImageBack().getHeight(null)))) {
+					SimpleAudioPlayer.playSyncSoundOnce(SoundProvider.getBubbleMenuClick(), 3f);
+
+					frame.drawPanel(EnumPanel.MENU_PANEL);
+				}
+			}
+
+		});
+
 	}
 
 	@Override
 	public void paintComponent(final Graphics g) {
 		super.paintComponent(g);
 		g.drawImage(scoreBackground, 0, 0, this.getWidth(), this.getHeight(), null);
+		g.drawImage(back, BACK_X, BACK_Y, 330, 83, null);
 
 	}
 }
